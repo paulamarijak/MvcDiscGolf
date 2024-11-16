c10a09480b9dcd3fc899bb4ebb0758f6a99cf08c
Paula Marija Kuročkina <p.m.kurockina@gmail.com>
2024-11-16 16:40:28 +0200

    Model added

---
+++ MvcDiscGolf/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcDiscGolf.Models
+{
+    public class Game
+    {
+        public int ID { get; set; }
+        public string Course { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public string Level { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class GameDBContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+}
