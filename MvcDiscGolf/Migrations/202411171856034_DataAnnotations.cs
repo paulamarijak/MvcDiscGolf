540e8f8fe79efe8160a2490d58b0169bf464e165
Paula Marija Kuročkina <p.m.kurockina@gmail.com>
2024-11-17 21:26:54 +0200

    Validation added

---
+++ MvcDiscGolf/Migrations/202411171856034_DataAnnotations.cs
@@ -0,0 +1,22 @@
+﻿namespace MvcDiscGolf.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DataAnnotations : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Games", "Course", c => c.String(maxLength: 60));
+            AlterColumn("dbo.Games", "Level", c => c.String(nullable: false, maxLength: 30));
+            AlterColumn("dbo.Games", "Rating", c => c.String(maxLength: 25));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Games", "Rating", c => c.String());
+            AlterColumn("dbo.Games", "Level", c => c.String());
+            AlterColumn("dbo.Games", "Course", c => c.String());
+        }
+    }
+}
--- MvcDiscGolf/Models/Game.cs
+++ MvcDiscGolf/Models/Game.cs
@@ -7,14 +7,26 @@ namespace MvcDiscGolf.Models
     public class Game
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Course { get; set; }
         
         [Display(Name = "Opening Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime OpeningDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Level { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [StringLength(25)]
         public string Rating { get; set; }
     }
     public class GameDBContext : DbContext
